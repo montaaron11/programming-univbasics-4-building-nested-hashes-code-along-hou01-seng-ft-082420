56b93026ffaaf1e5d1000395ba44a906254850da
Aaron Montgomery <montaaron11@gmail.com>
2020-07-24 01:11:27 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {"Lord Montague", "Lady Montague", "Romeo"}
+     :montague => {"Lord Montague", "Lady Montague", "Romeo"},
      :capulet => {"Lord Capulet", "Lady Capulet", "Juliet"}
    }
 end
